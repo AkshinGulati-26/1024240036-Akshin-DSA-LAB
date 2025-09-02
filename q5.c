57a86417d0acab8b4ca2fc3f1b55d6cc12129d4a
AkshinGulati-26 <agulati_be24@thapar.edu>
2025-09-02 12:01:11 +0530

    Update q5.c

--- q5.c
+++ q5.c
@@ -4,15 +4,23 @@
 int main() {
     int matrix[10][10], rows, cols, i, j, sumRow, sumCol;
     scanf("%d%d", &rows, &cols);
-    for (i = 0; i < rows; i++) for (j = 0; j < cols; j++) scanf("%d", &matrix[i][j]);
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+    }
     for (i = 0; i < rows; i++) {
         sumRow = 0;
-        for (j = 0; j < cols; j++) sumRow += matrix[i][j];
+        for (j = 0; j < cols; j++) {
+            sumRow += matrix[i][j];
+        }
         printf("Row %d sum: %d\n", i + 1, sumRow);
     }
     for (j = 0; j < cols; j++) {
         sumCol = 0;
-        for (i = 0; i < rows; i++) sumCol += matrix[i][j];
+        for (i = 0; i < rows; i++) {
+            sumCol += matrix[i][j];
+        }
         printf("Col %d sum: %d\n", j + 1, sumCol);
     }
 }
