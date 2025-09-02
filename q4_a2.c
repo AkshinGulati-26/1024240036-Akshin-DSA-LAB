daac0199993b7b149f843508ac2cdb82ba497d5f
AkshinGulati-26 <agulati_be24@thapar.edu>
2025-09-02 11:31:41 +0530

    Update q4_a2.c

--- q4_a2.c
+++ q4_a2.c
@@ -8,23 +8,25 @@ int main() {
     printf("Enter second string: ");
     scanf("%s", s2);
 
-    // a. Concatenate
+    // a
     strcat(s1, s2);
     printf("Concatenated: %s\n", s1);
 
-    // b. Reverse
+    //b
     int n = strlen(s1);
     for (int i = 0; i < n / 2; i++) {
-        char t = s1[i]; s1[i] = s1[n - i - 1]; s1[n - i - 1] = t;
+        char t = s1[i]; 
+        s1[i] = s1[n - i - 1]; 
+        s1[n - i - 1] = t;
     }
     printf("Reversed: %s\n", s1);
 
-    // c. Remove vowels
-    char temp[100]; int j = 0;
+    // c
+    char temp[100];
+    int j = 0;
     for (int i = 0; s1[i]; i++) {
         if (strchr("aeiouAEIOU", s1[i]) == NULL) temp[j++] = s1[i];
     }
-    temp[j] = '\0';
     printf("Without vowels: %s\n", temp);
 
     return 0;
