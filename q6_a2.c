973fa3a6e2d20b53e0060ba2e887a45cbb5ebba4
AkshinGulati-26 <agulati_be24@thapar.edu>
2025-09-02 11:16:36 +0530

    Update q6_a2.c

--- q6_a2.c
+++ q6_a2.c
@@ -7,17 +7,32 @@ int main() {
     int A[r][c], B[r][c], Sum[r][c], Trans[c][r];
 
     printf("Enter A: ");
-    for (int i = 0; i < r; i++) for (int j = 0; j < c; j++) scanf("%d", &A[i][j]);
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++){
+            scanf("%d", &A[i][j]);}
+    }
     printf("Enter B: ");
-    for (int i = 0; i < r; i++) for (int j = 0; j < c; j++) scanf("%d", &B[i][j]);
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++){
+            scanf("%d", &B[i][j]);
+        }
+    }
 
-    // a. Transpose A
-    for (int i = 0; i < r; i++) for (int j = 0; j < c; j++) Trans[j][i] = A[i][j];
+    // a
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            Trans[j][i] = A[i][j];
+        }
+    }
     printf("Transpose of A:\n");
     for (int i = 0; i < c; i++) { for (int j = 0; j < r; j++) printf("%d ", Trans[i][j]); printf("\n"); }
 
     // b. Addition
-    for (int i = 0; i < r; i++) for (int j = 0; j < c; j++) Sum[i][j] = A[i][j] + B[i][j];
+    for (int i = 0; i < r; i++)
+        {for (int j = 0; j < c; j++){
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+        }
     printf("A + B:\n");
     for (int i = 0; i < r; i++) { for (int j = 0; j < c; j++) printf("%d ", Sum[i][j]); printf("\n"); }
 
