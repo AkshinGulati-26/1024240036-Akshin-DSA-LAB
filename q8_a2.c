682d46ae3e49a49c5c60c2db0853c7398ec7a610
AkshinGulati-26 <agulati_be24@thapar.edu>
2025-09-02 11:13:25 +0530

    Update q8_a2.c

--- q8_a2.c
+++ q8_a2.c
@@ -10,13 +10,13 @@ int main() {
 
     int count = 0;
     for (int i = 0; i < n; i++) {
-        int seen = 0;
+        int seen=0;
         for (int j = 0; j < i; j++){
             if (arr[i] == arr[j]) {
              seen = 1; 
              break; 
             }
-        if (!seen) count++;
+         if (!seen) count++;
     }
 
     printf("Distinct count = %d\n", count);
